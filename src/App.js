d7d91f1a56aa0843413114072368800b05a3a4b5
Artem Kozhanov <artemkozhanov@MacBook-Air-Artem.local>
2023-01-27 21:35:07 +0300

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+
+        const [posts, setPosts] = useState([])
+
+        const [text, setText] = useState('')
+
+        const addCase = (e) => {
+            e.preventDefault()
+            const newTodo = {id: Date.now(), title: text}
+            setPosts([...posts, newTodo])
+            setText('')
+        }
+
+    const deleteBtn = (id) => {
+        console.log(id)
+        // console.log(posts)
+        setPosts(posts.filter(item => item.id !== id))
+
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>todoList</h2>
+        <div>
+            <input
+                value={text}
+                onChange={e => setText(e.target.value)}
+                type='text'
+                placeholder='введите текст'/>
+            <button disabled={!text} onClick={addCase}>добавить дело</button>
+        </div>
+        {posts.map((post) => {
+            return (
+                <div className='post'>
+                    <div className='postContent'>
+                        <strong>{post.title}</strong>
+                    </div>
+                    <div className='postBtn'>
+                        <button onClick={() => deleteBtn(post.id)} key={post.id}>delete</button>
+                    </div>
+                </div>
+                )
+            }
+        )}
     </div>
   );
 }
